b7f0de043000dd3bc58f1cd76c9ea8cdeb1e7289
sauravchandra1 <sauravchandra125@gmail.com>
2019-04-12 23:28:43 +0530

    some finishing

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var passport = require('passport');
+var { ensureAuthenticated } = require('../config/auth');
 
 //User model
 var User = require('../models/User');
@@ -47,50 +48,56 @@ router.get('/logout', (req, res) => {
 // });
 
 //Submit
-router.post('/submit', (req, res) => {
-    async function saveChoice(user) {
-        var student_id = user.student_id;
-        var cgpi = user.cgpi;
-        var branch = user.branch;
-        var subjects = await Subject.find({branch: {$nin: branch}}).exec();
-        if (subjects.length === 0) {
-            console.log('Subjects Not Found');
-        } else {
-            var subject_name = [];
-            var choices = [];
-            var sub;
-            var formData = req.body;
-            for (var i = 0; i < subjects.length; i++) {
-                sub = subjects[i].subject_name;
-                subject_name.push(sub);
-                choices.push(formData[sub]);
-            }
-            User.findOneAndUpdate({student_id: student_id}, {$set: {filled: true}}, {new: true}, (err, doc) => {
-                if (err) {
-                    console.log("Something Wrong While Updating Data!");
+router.post('/submit', ensureAuthenticated, (req, res) => {
+    var Userr = req.session.passport.user;
+    if (Userr.type === 1) {
+        req.logout();
+        res.redirect('/')
+    } else {
+        async function saveChoice(user) {
+            var student_id = user.student_id;
+            var cgpi = user.cgpi;
+            var branch = user.branch;
+            var subjects = await Subject.find({branch: {$nin: branch}}).exec();
+            if (subjects.length === 0) {
+                console.log('Subjects Not Found');
+            } else {
+                var subject_name = [];
+                var choices = [];
+                var sub;
+                var formData = req.body;
+                for (var i = 0; i < subjects.length; i++) {
+                    sub = subjects[i].subject_name;
+                    subject_name.push(sub);
+                    choices.push(formData[sub]);
                 }
-                console.log('Filled Set To True');
-                console.log(doc);
-            });
-            var newChoice = new Choice({
-                subject_name,
-                student_id,
-                choices,
-                cgpi
-            });
-            newChoice.save()
-                .then(choice => {
-                    console.log('Choices Filled Successfully');
-                    console.log(choice);
-                })
-                .catch(err => console.log(err));
+                User.findOneAndUpdate({student_id: student_id}, {$set: {filled: true}}, {new: true}, (err, doc) => {
+                    if (err) {
+                        console.log("Something Wrong While Updating Data!");
+                    }
+                    console.log('Filled Set To True');
+                    console.log(doc);
+                });
+                var newChoice = new Choice({
+                    subject_name,
+                    student_id,
+                    choices,
+                    cgpi
+                });
+                newChoice.save()
+                    .then(choice => {
+                        console.log('Choices Filled Successfully');
+                        console.log(choice);
+                    })
+                    .catch(err => console.log(err));
+            }
         }
-    }
 
-    saveChoice(req.session.passport.user);
-    req.logout();
-    req.flash('success_msg', 'You Have Filled The Choices Successfully');
-    res.redirect('/users/student_login');
+        saveChoice(req.session.passport.user);
+        req.logout();
+        req.flash('success_msg', 'You Have Filled The Choices Successfully');
+        res.redirect('/users/student_login');
+    }
 });
 
 
